72b75f2ca208b7d02fef002da3de2295d78b636a
bahhh777 <cayman777@mail.ru>
2017-02-07 00:37:40 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Павел - учвстник курса НЕТОЛОГИЯ</title>
+	  <style>
+            body {
+                background: #efefef;
+                font-family: sans-serif;  
+            }
+        </style>
+</head>
+<body>
+<?php
+$myname = 'Павел';
+$age = '28';
+$mymail = 'www.cayman777@mail.ru'; 
+$mycity = 'Москва';
+$aboutmy = 'Меня зовут Павел, я участник курса "PHP/SQL:back-end разработка и базы данных.';
+?>
+<div>
+	<h1>Страница пользователя Павла</h1>
+	<p>Имя: <strong><?= $myname ?></strong></p>
+	<p>Возраст: <strong><?= $age ?></strong></p>
+	<p>Адрес электронной почты:  так: <a href="mailto:cayman777@mail.ru"><?= $mymail ?></a></p>
+	<p>Город: <strong><?= $mycity ?> </strong></p>
+	<p>О себе: <strong><?= $aboutmy ?></strong></p>
+</div>
+<?php
+$myname = 'Павел';
+$age = '28';
+$mycity = 'Москва';
+/*<a href="mailto:cayman777@mail.ru">*/
+?>
+</body>
+</html>
